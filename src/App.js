951e6efe01a57bdd5dcc216fe07179ddbe02af19
Chad <ckennedy9231988@gmail.com>
2021-01-09 19:29:06 -0600

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useRef, useEffect} from 'react'
+import TaskList from './Components/TaskList'
+import { v4 as uuidv4} from 'uuid'
+import './css/app.css'
+
+const LOCAL_STORAGE_KEY = 'taskApp.tasks'
 
 function App() {
+  
+  //state for tasks to go in
+  const [tasks, setTasks] = useState([]);
+  const taskNameRef = useRef();
+
+  //useEffect to load Tasks from the local storage
+  //Todo: we need to pull this from Database Eventually
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTasks) setTasks(storedTasks)
+
+  }, [])
+
+  //Toggling the Task Completed
+  function toggleTask(id){
+    const newTasks = [...tasks]
+    const task = newTasks.find(task => task.id === id)
+    task.complete = !task.complete
+    setTasks(newTasks)
+  }
+
+
+  //useEffect to save the tasks array to the local storage when it is changed
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(tasks))
+  }, [tasks])
+  
+
+  //add task function
+  function handleAddTask(e) {
+      const name = taskNameRef.current.value
+      if (name === '') return
+      setTasks(prevTasks => {
+        return [...prevTasks, { id: uuidv4(), name: name, complete: false}]
+      })
+      taskNameRef.current.value = null;
+  }
+
+  //delete task function
+  function deleteTask(id) {
+    const newTasks = [...tasks]
+    const remainingTasks = newTasks.filter(task => task.id !== id)
+    console.log(remainingTasks)
+    setTasks(remainingTasks)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Background">
+        <div className="Glass-Pane">
+          <input className="Add-Task-Text" type="text" placeholder="Enter the Task Name" ref={taskNameRef}></input>
+          <button className="Add-Task-Button" onClick={handleAddTask}>Add</button>
+          <div className="Task-Container">
+            <TaskList tasks={tasks} toggleTask={toggleTask} deleteTask={deleteTask}/>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
---
+++ src/Components/Task.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import '../css/task.css'
+
+
+function Task({ task, toggleTask, deleteTask }) 
+{
+    function handleTaskClick() {
+        toggleTask(task.id)
+    }
+    
+    function handleDeleteTask() {
+        deleteTask(task.id)
+    }
+
+    return (
+
+        <div className="task">  
+            <input id={task.name} className="Task-Completed" type="checkbox" checked={task.complete} onChange={handleTaskClick}></input>
+            <label for={task.name} className="Task-Name" style={{'text-decoration': task.complete ? 'line-through' : 'none'}}>{task.name}</label>      
+            <DeleteOutlinedIcon className="Task-Delete-Icon" onClick={handleDeleteTask}/>
+
+        </div>
+
+    )
+    
+}
+
+export default Task
